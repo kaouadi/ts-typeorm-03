74ebab99b74114be1213c2390550ad055f3fe391
Karim Aouadi <kdolphinus@gmail.com>
2020-03-31 19:12:32 +0200

    updated migration files

--- src/migration/1585240257355-CreateUser.ts
+++ src/migration/1585240257355-CreateUser.ts
@@ -12,3 +12,80 @@ export class CreateUser1585240257355 implements MigrationInterface {
     }
 
 }
+
+
+
+/*
+
+
+import {MigrationInterface, QueryRunner, Table, TableIndex, TableColumn, TableForeignKey } from "typeorm";
+
+export class QuestionRefactoringTIMESTAMP implements MigrationInterface {
+
+    async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "question",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                }
+            ]
+        }), true)
+
+        await queryRunner.createIndex("question", new TableIndex({
+            name: "IDX_QUESTION_NAME",
+            columnNames: ["name"]
+        }));
+
+        await queryRunner.createTable(new Table({
+            name: "answer",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                }
+            ]
+        }), true);
+
+        await queryRunner.addColumn("answer", new TableColumn({
+            name: "questionId",
+            type: "int"
+        }));
+
+        await queryRunner.createForeignKey("answer", new TableForeignKey({
+            columnNames: ["questionId"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "question",
+            onDelete: "CASCADE"
+        }));
+    }
+
+    async down(queryRunner: QueryRunner): Promise<void> {
+        const table = await queryRunner.getTable("question");
+        const foreignKey = table.foreignKeys.find(fk => fk.columnNames.indexOf("questionId") !== -1);
+        await queryRunner.dropForeignKey("question", foreignKey);
+        await queryRunner.dropColumn("question", "questionId");
+        await queryRunner.dropTable("answer");
+        await queryRunner.dropIndex("question", "IDX_QUESTION_NAME");
+        await queryRunner.dropTable("question");
+    }
+
+}
+
+
+
+
+
+
+*/
