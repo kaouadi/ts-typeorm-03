6e4c7a67cd5bf34d918e4d7a55a1a76fe8699866
Nicolas <nicomouss@users.noreply.github.com>
2020-04-02 19:48:36 +0545

    Added User2.ts

---
+++ src/entity/User2.ts
@@ -0,0 +1,20 @@
+
+export class User2 {
+
+    public id: number;
+
+    public firstName: string;
+
+    public lastName: string;
+
+    public age: number;
+
+
+    constructor() {
+        this.id = null;
+        this.firstName = null;
+        this.lastName = null;
+        this.age = null;
+    }
+
+}
